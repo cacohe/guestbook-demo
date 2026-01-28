c435ec95c52d50bc254e41cadf743d8c1248e2a0
hpj <2568545091@qq.com>
2026-01-28 19:51:59 +0800

    bugfix

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,6 +1,6 @@
 'use server'
 
-import { supabase } from '@/lib/supabase';
+import { createClient } from '@/lib/supabase';
 import { revalidatePath } from 'next/cache';
 
 
@@ -11,6 +11,7 @@ export async function addMessage(formData: FormData) {
   if (!content) return;
 
   // 插入数据到 Supabase
+  const supabase = await createClient()
   await supabase.from('messages').insert([{ content }]);
 
   // 重新验证页面，让页面立即刷新显示新数据
